4dc503ff3a18e0d611212a5b13e9b8e674d8a471
xjhqre <859425698@qq.com>
2023-10-27 14:51:29 +0800

    搜索页面历史记录布局

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -8,6 +8,24 @@
           <input type="text" placeholder="搜索歌曲"/>
           <text class="iconfont iconguanbi"></text>
         </view>
+        <view class="search-history">
+          <view class="search-history-head">
+            <text>历史记录</text>
+            <text class="iconfont iconlajitong"></text>
+          </view>
+          <view class="search-history-list">
+            <view>测试</view>
+            <view>测试</view>
+            <view>测试</view>
+            <view>测试</view>
+            <view>测试</view>
+            <view>测试</view>
+            <view>测试</view>
+            <view>测试</view>
+
+
+          </view>
+        </view>
       </scroll-view>
     </view>
   </view>
@@ -43,5 +61,29 @@ export default {
 .search-search input {
   flex: 1;
   font-size: 13px;
+}
+
+.search-history {
+  margin: 0 15px;
+  font-size: 13px;
+}
+
+.search-history-head {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 18px;
+}
+
+.search-history-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.search-history-list view {
+  padding: 8px 14px;
+  border-radius: 20px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  background: #f7f7f7;
 }
 </style>
